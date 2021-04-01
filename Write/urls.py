44489f439771a610e7a34bf06e39e33c7950caa3
Lee Jong Ann <dlwhddksiq@naver.com>
2021-04-01 21:34:22 +0900

    로그인 URL 수정 및 로그인 템플릿 에러 추가

--- Write/urls.py
+++ Write/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',views.index),
     path('question_create', views.question_create,name='question_create'),
     path('index',views.index,name = 'index'),
-    path('login',views.login,name = 'login'),
+    # path('login',views.login,name = 'login'),
     path('detail/<int:question_id>',views.detail,name = 'detail'),
     path('answer_create/<int:question_id>',views.answer_create,name='answer_create')
 ]
--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,11 @@
 
-from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
-app_name = 'login'
+
+
+app_name = 'common'
 urlpatterns = [
 
-
+    path('login/',auth_views.LoginView.as_view(template_name='common/login.html'),name= 'login'),
 ]
