bf8e6763abc82a2a898c39ab181430eacb4b4435
Lee Jong Ann <dlwhddksiq@naver.com>
2021-06-07 15:23:53 +0900

    파일 업로드 테스트

--- pybo/urls.py
+++ pybo/urls.py
@@ -49,5 +49,4 @@ urlpatterns = [
     # path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
